16ff0202bb534b77bab973e81df88effd4913503
ibrahim köprücü <i.koprucu07@gmail.com>
2018-10-12 00:29:57 +0300

    Calculator form added and simple operations implemented.

---
+++ SimpleCalculator/Calculator.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace SimpleCalculator
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFirstNumber = new System.Windows.Forms.TextBox();
+            this.txtSecondNumber = new System.Windows.Forms.TextBox();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtFirstNumber
+            // 
+            this.txtFirstNumber.Location = new System.Drawing.Point(26, 37);
+            this.txtFirstNumber.Name = "txtFirstNumber";
+            this.txtFirstNumber.Size = new System.Drawing.Size(117, 20);
+            this.txtFirstNumber.TabIndex = 0;
+            // 
+            // txtSecondNumber
+            // 
+            this.txtSecondNumber.Location = new System.Drawing.Point(159, 37);
+            this.txtSecondNumber.Name = "txtSecondNumber";
+            this.txtSecondNumber.Size = new System.Drawing.Size(117, 20);
+            this.txtSecondNumber.TabIndex = 0;
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Location = new System.Drawing.Point(26, 67);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(23, 23);
+            this.btnPlus.TabIndex = 1;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(26, 123);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(250, 20);
+            this.txtResult.TabIndex = 0;
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Location = new System.Drawing.Point(113, 66);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(23, 23);
+            this.btnDivide.TabIndex = 3;
+            this.btnDivide.Text = "/";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Location = new System.Drawing.Point(84, 66);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(23, 23);
+            this.btnMultiply.TabIndex = 4;
+            this.btnMultiply.Text = "x";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.Location = new System.Drawing.Point(55, 67);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(23, 23);
+            this.btnMinus.TabIndex = 5;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Sonuç:";
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 224);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtSecondNumber);
+            this.Controls.Add(this.txtFirstNumber);
+            this.Name = "Calculator";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFirstNumber;
+        private System.Windows.Forms.TextBox txtSecondNumber;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ SimpleCalculator/Calculator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SimpleCalculator
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        // violation of DRY (Don't Repeat Yourself) principle
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            string firstNumberText = txtFirstNumber.Text;
+            string secondNumberText = txtSecondNumber.Text;
+
+            int firstNumber = Convert.ToInt32(firstNumberText);
+            int secondNumber = Convert.ToInt32(secondNumberText);
+
+            txtResult.Text = (firstNumber + secondNumber).ToString();
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            string firstNumberText = txtFirstNumber.Text;
+            string secondNumberText = txtSecondNumber.Text;
+
+            int firstNumber = Convert.ToInt32(firstNumberText);
+            int secondNumber = Convert.ToInt32(secondNumberText);
+
+            txtResult.Text = (firstNumber - secondNumber).ToString();
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            string firstNumberText = txtFirstNumber.Text;
+            string secondNumberText = txtSecondNumber.Text;
+
+            int firstNumber = Convert.ToInt32(firstNumberText);
+            int secondNumber = Convert.ToInt32(secondNumberText);
+
+            txtResult.Text = (firstNumber * secondNumber).ToString();
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            string firstNumberText = txtFirstNumber.Text;
+            string secondNumberText = txtSecondNumber.Text;
+
+            int firstNumber = Convert.ToInt32(firstNumberText);
+            int secondNumber = Convert.ToInt32(secondNumberText);
+
+            txtResult.Text = (firstNumber / secondNumber).ToString();
+        }
+
+    }
+}
